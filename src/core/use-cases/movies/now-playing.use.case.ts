78143f5b319513fa642aef401e85013c8b4c5625
braian <brasanluc123@gmail.com>
2024-04-25 20:10:52 -0300

    I repeat class 146; use case Now playing

---
+++ src/core/use-cases/movies/now-playing.use.case.ts
@@ -0,0 +1,16 @@
+import {HttpAdapter} from '../../../config/adapters/http/http.adapter';
+import {NowPlayingResponse} from '../../../infrastrustura/interfaces/movie-db.responses';
+import type {Movie} from '../../entities/movie.entiti';
+
+export const moviesNowPlayingUseCase = async (
+  fetcher: HttpAdapter,
+): Promise<Movie[]> => {
+  try {
+    const nowPlaying = await fetcher.get<NowPlayingResponse>('/now_playing');
+    console.log({nowPlaying});
+
+    return [];
+  } catch (error) {
+    throw new Error('Error fetching movies - NowPlaying');
+  }
+};
