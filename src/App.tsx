bb97c6add487d1fefcd5db8f8b31cc4af7c5b216
braian <brasanluc123@gmail.com>
2024-04-25 12:38:17 -0300

    I repeat class 143; installing global navigation with its dependencies and a type of Stack browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Navigation} from './presentation/navigations/Navigation';
 
 export const App = () => {
   return (
-    <View>
-      <Text>MoviesApp</Text>
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 };
---
+++ src/presentation/navigations/Navigation.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screen/home/HomeScreen';
+import {DetailsScreen} from '../screen/details/DetailsScreen';
+
+export type RootStackParams = {
+  Home: undefined;
+  Details: {movieId: number};
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
