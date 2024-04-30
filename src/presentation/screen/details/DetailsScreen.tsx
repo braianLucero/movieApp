068dea507a3da8ad6a76f2b8f11f3b4c1809c493
braian <brasanluc123@gmail.com>
2024-04-30 18:38:58 -0300

    clas 162. saving movie information by ID

--- src/presentation/screen/details/DetailsScreen.tsx
+++ src/presentation/screen/details/DetailsScreen.tsx
@@ -1,7 +1,15 @@
+// import {useRoute} from '@react-navigation/native';
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text} from 'react-native';
+import {RootStackParams} from '../../navigations/Navigation';
 
-export const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailsScreen = ({route}: Props) => {
+  const {movieId} = route.params;
+  // const {movieId} = useRoute().params;
+
   return (
     <View>
       <Text>Details Screen</Text>
