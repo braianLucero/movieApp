e3d0606ad6e9c9da6ea7f53c16285c157233be3c
braian <brasanluc123@gmail.com>
2024-04-30 20:52:23 -0300

    clas 164.adding styles and details to the Header

--- src/presentation/screen/details/DetailsScreen.tsx
+++ src/presentation/screen/details/DetailsScreen.tsx
@@ -3,16 +3,23 @@ import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View, Text} from 'react-native';
 import {RootStackParams} from '../../navigations/Navigation';
+import {useMovie} from '../../hooks/useMovie';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 
 export const DetailsScreen = ({route}: Props) => {
   const {movieId} = route.params;
-  // const {movieId} = useRoute().params;
+  // const {movieId} = useRoute().par
+
+  const {isLoading, movie} = useMovie(movieId);
+
+  if (isLoading) {
+    return <Text>Loading..</Text>;
+  }
 
   return (
     <View>
-      <Text>Details Screen</Text>
+      <MovieHeader movie={movie!} />
     </View>
   );
 };
