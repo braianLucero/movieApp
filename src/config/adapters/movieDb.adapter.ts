75b324fa46193c00084d8b18ecad52bdf0e33c82
braian <brasanluc123@gmail.com>
2024-04-30 23:26:12 -0300

    class 174. Project start - ComponentsAp

--- src/config/adapters/movieDb.adapter.ts
+++
@@ -1,9 +0,0 @@
-import {AxiosAdapter} from './http/axios.adapter';
-
-export const movieDBFetcher = new AxiosAdapter({
-  baseUrl: 'https://api.themoviedb.org/3/movie',
-  params: {
-    api_key: '731f24a408a70c6bd83f9cb1cf12d64e',
-    lenguage: 'es',
-  },
-});
--- src/core/use-cases/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './movies/now-playing.use.case';
-export * from './movies/Popular.use.case';
-export * from './movies/top-rated.use.case';
-export * from './movies/upcoming.use.case';
